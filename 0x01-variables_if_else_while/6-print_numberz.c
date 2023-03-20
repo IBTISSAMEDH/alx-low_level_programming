7ab21c5917ba56f6a6559a02fed84f8bde081c09
IBTISSAMEDH <ibtissame.dahhoum@gmail.com>
2023-03-19 19:14:15 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all digits of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	printf("\n");
+	return (0);
+	}
